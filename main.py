1755859e92f529d435ff4759c1edc09144263c72
Tatiana Brimm <tbrimm1@student.gsu.edu>
2025-06-27 21:33:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -63,3 +63,7 @@ def chat_with_brimmbot(message: Message):
     )
 
     return {"reply": response.choices[0].message.content}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
